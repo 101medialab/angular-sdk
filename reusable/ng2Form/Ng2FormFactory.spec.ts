761ea5a082c3694d509858f95da7cc5e59a9aab5
tom10271 <tom.lo@101medialab.com>
2017-08-28 09:36:57 +0800

    Fix NgFormFactory test case

--- reusable/ng2Form/Ng2FormFactory.spec.ts
+++ reusable/ng2Form/Ng2FormFactory.spec.ts
@@ -9,42 +9,115 @@ describe('Ng2FormFactory.generateFormGroupByAttributeTypeObject', () => {
         let expected = Factory.generateFormGroupByOATMapping(
             new FormBuilder(),
             expectedMapping
-        );
+        ).templateConfig;
 
         expect(
             expected
-        ).toEqual({
-            "templateConfig": {
-                "anyAttributeName": {
-                    "label": "Any Attribute Name",
-                    "renderType": "text",
+        ).toMatchObject({
+            "anyAttributeName": {
+                "label": "Any Attribute Name",
+                "renderType": "text",
+                "type": "string"
+            },
+            "booleanAttributeName": {
+                "label": "Boolean Attribute Name",
+                "renderType": "checkbox",
+                "type": "boolean"
+            },
+            "dateAttributeName": {
+                "label": "Date Attribute Name",
+                "renderType": "date",
+                "type": "date"
+            },
+            "objectArrayAttributeName": {
+                "children": {
+                    "anyAttributeName": {
+                        "label": "Any Attribute Name",
+                        "renderType": "text",
+                        "type": "string"
+                    },
+                    "booleanAttributeName": {
+                        "label": "Boolean Attribute Name",
+                        "renderType": "checkbox",
+                        "type": "boolean"
+                    },
+                    "dateAttributeName": {
+                        "label": "Date Attribute Name",
+                        "renderType": "date",
+                        "type": "date"
+                    },
+                    "objectArrayAttributeName": {
+                        "children": {
+                            "attr1": {
+                                "label": "Attr1",
+                                "renderType": "number",
+                                "type": "number"
+                            }
+                        },
+                        "groupType": "object",
+                        "label": "Object Array Attribute Name"
+                    },
+                    "objectAttributeName": {
+                        "children": {
+                            "attr1": {
+                                "label": "Attr1",
+                                "renderType": "number",
+                                "type": "number"
+                            }
+                        },
+                        "groupType": "object",
+                        "label": "Object Attribute Name"
+                    },
+                    "primitiveArrayAttributeName": {
+                        "arrayType": "primitive",
+                        "children": [
+                            {
+                                "primitiveArrayAttributeName": {
+                                    "label": "Primitive Array Attribute Name",
+                                    "renderType": "number",
+                                    "type": "number"
+                                }
+                            }
+                        ],
+                        "groupType": "array",
+                        "label": "Primitive Array Attribute Name",
+                        "renderType": "array"
+                    },
+                    "stringAttributeName": {
+                        "label": "String Attribute Name",
+                        "renderType": "text",
+                        "type": "string"
+                    }
                 },
-                "booleanAttributeName": {
-                    "label": "Boolean Attribute Name",
-                    "renderType": "checkbox",
-                    "type": "boolean",
+                "groupType": "object",
+                "label": "Object Array Attribute Name"
+            },
+            "objectAttributeName": {
+                "children": {
+                    "attr1": {
+                        "label": "Attr1",
+                        "renderType": "number",
+                        "type": "number"
+                    }
                 },
-                "dateAttributeName": {
-                    "label": "Date Attribute Name",
-                    "renderType": "date",
-                    "type": "date",
+                "groupType": "object",
+                "label": "Object Attribute Name"
+            },
+            "primitiveArrayAttributeName": {
+                "children": {
+                    "primitiveArrayAttributeName": {
+                        "label": "Primitive Array Attribute Name",
+                        "renderType": "number",
+                        "type": "number"
+                    }
                 },
-                "objectArrayAttributeName": {
-                    "label": "Object Array Attribute Name",
-                    "renderType": "text",
-                },
-                "objectAttributeName": {
-                    "label": "Object Attribute Name",
-                    "renderType": "text",
-                },
-                "primitiveArrayAttributeName": {
-                    "label": "Primitive Array Attribute Name",
-                    "renderType": "text",
-                },
-                "stringAttributeName": {
-                    "label": "String Attribute Name",
-                    "renderType": "text",
-                }
+                "groupType": "object",
+                "label": "Primitive Array Attribute Name"
+            },
+            "stringAttributeName": {
+                "label": "String Attribute Name",
+                "renderType": "text",
+                "type": "string"
             }
         });
     });
